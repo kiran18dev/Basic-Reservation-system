4233ed86d6120abe6fb7d4c6556fc86666c50ebe
KIRANMAI-PC\KIRANMAI <kiran18.ui@gmail.com>
2017-10-09 17:41:39 -0500

    adding config.js file for configuration on environments

---
+++ config.js
@@ -0,0 +1,6 @@
+var PORT = process.env.PORT || 3333;
+var env = process.env.NODE_ENV || 'development';
+module.exports = {
+	PORT: PORT,
+	env:env
+}
--- server.js
+++ server.js
@@ -3,9 +3,8 @@ var express = require('express')
 var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
-const PORT = process.env.PORT || 3333;
-
-const routes = require('./routes');
+var config = require('./config');
+var routes = require('./routes');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,6 +15,12 @@ mongoUtil.connectToServer( function( err ) {
 	app.use('/', routes);
 });
 
-app.listen(PORT, function () {
-	console.log('server is running at http://localhost:'+PORT)
+/*
+if (config.env === 'production') {
+	// do something;
+}
+*/
+
+app.listen(config.PORT, function () {
+	console.log('server is running at http://localhost:'+config.PORT)
 });
